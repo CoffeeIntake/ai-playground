b649f5e67a9fdb30ca35bae3c0cb56a0526555b1
timj-at-sage <=>
2019-11-04 11:41:11 -0800

    Adding model builder project files that were missing 	new file:   ConsoleApp1/ConsoleApp1ML.ModelBuilder/ConsoleApp1ML.ModelRebuilder.csproj 	new file:   ConsoleApp1/ConsoleApp1ML.ModelBuilder/Program.cs

---
+++ ConsoleApp1/ConsoleApp1ML.ModelBuilder/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1ML.ModelRebuilder
+{
+    using ConsoleApp1ML.ConsoleApp;
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Rebuilding model");
+
+            // Rebuild the model
+            ModelBuilder.CreateModel();
+
+            Console.WriteLine("Model has been rebuilt");
+        }
+    }
+}
