c4bab70f5c5eaff2b672a732d47892e4c093eb5b
timj-at-sage <56836983+timj-at-sage@users.noreply.github.com>
2019-11-12 05:53:18 -0800

    Tim add movie recommendations (#6)
    * checking in initial dockerfile and dockerignore.
    
    * Creating container images for running our ML console app
    
    * Fixed new line at end of docker file
    
    * Fixed ModelBuilder.TRAIN_DATA_FILEPATH to use a relative link instead of a static path on Alex's machine.
    
    * Removed testing code from Main
    
    * Reverted testing code from Model Input
    
    * Added console app to allow model retraining
    
    * Renamed ModelRebuilder for better consistency with other projects in solution, and for more clarity as to its function
    
    * Added movie recommendations sample from tutorial
    
    * Added MovieRecomender solution, migrated project
    
    * Fixing ConsoleApp1.sln
    
    * Removing MovieRecommendation from ConsoleApp1.sln to allow builds to be separated

---
+++ MovieRecommender/MovieRecommender/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MovieRecommender
+{
+    using System;
+    using System.IO;
+    using Microsoft.ML;
+    using Microsoft.ML.Trainers;
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModelBuilder.CreateModel();
+        }        
+    }
+}
