c9e43de8b3a1a7ecf2792024971efa69fa27964d
timj-at-sage <=>
2019-11-12 17:33:20 -0800

    Adding in data access to 300 CRE 	modified:   ConsoleApp1.sln 	new file:   InvoiceData/Configuration.cs 	new file:   InvoiceData/Customer.cs 	new file:   InvoiceData/InvoiceData.csproj 	new file:   InvoiceData/Lists.cs 	new file:   InvoiceData/Properties/AssemblyInfo.cs 	new file:   InvoiceData/config.json 	new file:   InvoiceData/packages.config 	new file:   InvoiceDataTester/App.config 	new file:   InvoiceDataTester/InvoiceDataTester.csproj 	new file:   InvoiceDataTester/Program.cs 	new file:   InvoiceDataTester/Properties/AssemblyInfo.cs 	new file:   InvoiceDataTester/packages.config

---
+++ ConsoleApp1/InvoiceData/Lists.cs
@@ -0,0 +1,76 @@
+﻿namespace InvoiceData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Data;
+    using System.Data.Odbc;
+    using System.Reflection;
+
+    public class ListBase
+    {
+        public static IEnumerable<T> GetList<T>(string query)
+        {
+            List<T> results = new List<T>();
+
+            string odbcDriver = @"{TIMBERLINE DATA}";
+            string dataFolderPath = @"C:\repos\source\Main\Library Sets\STO\Libraries\Legacy\DemoData\Ext GC DemoData\";
+            string userName = "1";
+            string password = userName;
+
+            var connectionString = System.String.Format(System.Globalization.CultureInfo.InvariantCulture,
+                @"Driver={0};dbq={1};uid={2};pwd={3};codepage=1252;shortennames=0;standardmode=1;maxcolsupport=1536",
+                odbcDriver, dataFolderPath, userName, password);
+
+            using (var connection = new OdbcConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandTimeout = 100;
+                        command.CommandText = query;
+
+                        using (var reader = command.ExecuteReader())
+                        {   
+                            while (reader.Read())
+                            {
+                                var result = (T)Activator.CreateInstance(typeof(T));
+                                var properties = result.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+                                try
+                                {
+                                    // TODO: Check types on the properties I have.  Good to commit for now.
+                                    foreach (var property in properties)
+                                    {
+                                        property.SetValue(result, reader[property.Name.Replace(" ","_")]);
+                                    }
+
+                                }
+                                catch (Exception ex)
+                                {
+                                    string message = "Handle errors better: " + ex.ToString();
+                                    Console.WriteLine(message);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    string message = "Handle errors better here, too: " + ex.ToString();
+                    Console.WriteLine(message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+
+            return results;
+        }
+    }
+}
---
+++ ConsoleApp1/InvoiceDataTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using InvoiceData;
+
+namespace InvoiceDataTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string query = "SELECT * FROM [ABM_MASTER__CONTACT]";
+            var customer = InvoiceData.ListBase.GetList<Customer>(query);
+        }
+    }
+}
